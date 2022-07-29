34902eaf444a7fb499aecde6e44014e914f2d2ff
Louise Lalanne <100588945+louiselalanne@users.noreply.github.com>
2022-07-29 09:05:09 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -2,9 +2,9 @@ import hashlib
 import os
 import base64
 
-with open("password.lst", "r") as f:
-    temp = f.read().splitlines()
-    for each_element in temp:
+with open("password.lst", "r") as f: #abrir aquivo
+    temp = f.read().splitlines() #ler o arquivo e dividir em linhas
+    for each_element in temp: #para cada palavra em uma linha
         result = hashlib.md5(each_element.encode("utf-8")).hexdigest()
         base = base64.b64encode(result.encode())
         sha = hashlib.sha1(base).hexdigest()
